f93596d5518c390ee41f36e7e3889001b1f1bc5f
santhossivan1206 <2019cs0452@svce.ac.in>
2022-11-25 23:08:12 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Transactions from './transactions.json';
 
 function App() {
+  var answer=""
+  fetch('https://jsonplaceholder.typicode.com/posts')
+   .then(response => response.json())
+   .then(data => answer = data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br/><br/><br/><br/>
+       <h1>SMART CONTRACT TRANSACTIONS</h1>
+       <br/><br/>
+
+       {
+          Transactions.map( transactions => {
+            return(
+              
+              <div className="box" key={transactions.id}>
+
+                  <strong>{" Product Name: "+transactions.productname}</strong>
+                  <div className="box2">{" Product Price: "+transactions.productprice}</div>
+              </div>
+            )
+          })
+       }
     </div>
   );
 }
